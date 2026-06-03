4d65b8c7bf7e2643af982ed6ca389c4c248f96f1
jd156318 <shiloh.gunton@my.jcu.edu.au>
2026-06-03 11:17:33 +1000

    Add random score generator and determine prize qualification

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,12 +2,21 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+from random import randint
 
 
 def main():
+    # Determine and print score status for user input
     score = float(input("Enter score: "))
     status = determine_score_status(score)
-    print(status)
+    print(f"User status {score} is {status}")
+    if 100 >= score >= 90:
+        print("You get a prize!")
+
+    # Determine and print score status for random input
+    score = randint(0, 100)
+    status = determine_score_status(score)
+    print(f"Random: {score} = {status}")
 
 
 def determine_score_status(score):
