a02758b5a779073376c6a4c6f77c7dafa0e743e5
jd156318 <shiloh.gunton@my.jcu.edu.au>
2026-06-05 09:25:25 +1000

    Add randoms.py program to investigate random module

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+import random
+
+print(random.randint(5, 20))
+# 7 - smallest = 5, largest = 20
+print(random.randrange(3, 10, 2))
+# 9 - smallest = 3, largest = 9. It could not have produced a 4
+print(random.uniform(2.5, 5.5))
+# 3.6131651747146445 - smallest = 2.5, largest = 5.5
+
+print(random.randint(1, 101))
