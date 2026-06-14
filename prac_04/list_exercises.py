bf200b93fb78a85ee00667e114b829fb3cbe418d
jd156318 <shiloh.gunton@my.jcu.edu.au>
2026-06-14 15:40:22 +1000

    Add program that outputs key information about user inputted numbers

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,13 @@
+numbers = []
+
+for i in range(5):
+    numbers.append(int(input("Enter number: ")))
+
+for i in range(len(numbers)):
+    print(f"Number: {numbers[i]}")
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
