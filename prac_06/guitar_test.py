41fc7d81922ac1b9e6f83e715ec14923f39c6feb
jd156318 <shiloh.gunton@my.jcu.edu.au>
2026-07-14 12:09:11 +1000

    Update guitar_test.py to test each Guitar method individually

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -6,6 +6,12 @@ Import Guitar class and test it
 from prac_06.guitar import Guitar
 
 gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+other = Guitar("Another Guitar", 2013, 6450.12)
+
 print(gibson)
-print(gibson.get_age())
-print(gibson.is_vintage())
+print(other)
+
+print(f"{gibson.name} get_age() - Expected 104. Got {gibson.get_age()}")
+print(f"{other.name} get_age() - Expected 13. Got {other.get_age()}")
+print(f"{gibson.name} is_vintage - Expected True. Got {gibson.is_vintage()}")
+print(f"{other.name} is_vintage - Expected False. Got {other.is_vintage()}")
