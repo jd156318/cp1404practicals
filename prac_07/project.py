993db087d6034dc566f7f0623ed3bbbd072f295d
jd156318 <shiloh.gunton@my.jcu.edu.au>
2026-07-18 11:24:47 +1000

    Add menu choices S, L and update read_file_into_projects_list()

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,14 +3,29 @@ Practical CP1404
 Guitar class with name, start date, priority, cost estimate, and project completion percentage.
 """
 
+from datetime import datetime
+
 
 class Project:
     """Project class for storing data about a project."""
 
-    def __init__(self, name, start_date, priority, cost, completion_percentage):
+    def __init__(self, name="", start_date="00/00/0000", priority=0, cost=0.0, completion_percentage=0):
         """Initialise a Project."""
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.cost = cost
         self.completion_percent = completion_percentage
+
+    def is_after_date(self, input_date):
+        """Return true if the project started after the input_date."""
+        return self.start_date > input_date
+
+    def __str__(self):
+        """Format object data to print neatly."""
+        return (f"{self.name}, start: {datetime.strftime(self.start_date, "%d/%m/%Y")}, priority {self.priority}, "
+                f"estimate: ${self.cost}, completion: {self.completion_percent}%")
+
+    def __lt__(self, other):
+        """Self is less than other if it has an earlier start date."""
+        return self.start_date < other.start_date
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,6 +4,7 @@ Estimate: 1 hour
 start: 19+2:56
 
 """
+from datetime import datetime
 from typing import Any
 
 from prac_07.project import Project
@@ -15,13 +16,50 @@ def main():
     print("Welcome to Pythonic Project Management")
     projects = read_file_into_projects_list(DEFAULT_FILE)
     choice = get_menu_choice()
+    while choice != "Q":
+        if choice == "L":
+            load_filename = input("Load file: ")
+            projects.append(load_filename)
+        if choice == "S":
+            save_project_data_to_file(projects)
+        if choice == "D":
+            pass
+        if choice == "F":
+            filter_project_objects_by_date(projects)
+        if choice == "A":
+            pass
+        if choice == "U":
+            pass
+        choice = get_menu_choice()
+
+
+def save_project_data_to_file(projects: list[Any]):
+    """Save project data to user specified filename."""
+    save_filename = input("Save to file: ")
+    with open(save_filename, "w", newline="") as out_file:
+        for project in projects:
+            out_file.write(f"{project.name}\t{datetime.strftime(project.start_date, "%d/%m/%Y")}\t"
+                           f"{project.priority}\t{project.cost}\t{project.completion_percent}\n")
+
+
+def filter_project_objects_by_date(projects: list[Any]):
+    """Print project objects that started after user specified date."""
+    filtered_projects = []
+    date_string = input("Show projects that start after date (dd/mm/yy): ")
+    date = datetime.strptime(date_string, "%d/%m/%Y").date()
+    for project in projects:
+        if project.is_after_date(date):
+            filtered_projects.append(project)
+    filtered_projects.sort()
+    for i in range(len(filtered_projects)):
+        print(filtered_projects[i])
 
 
 def get_menu_choice():
     """Print menu and get user selection."""
     print("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project"
           "\n- (U)pdate project\n- (Q)uit")
-    return input(">>> ")
+    return input(">>> ").upper()
 
 
 def read_file_into_projects_list(filename) -> list[Any]:
@@ -31,12 +69,10 @@ def read_file_into_projects_list(filename) -> list[Any]:
         in_file.readline()  # Don't read header line in file
         lines = in_file.readlines()
         for line in lines:
-            parts = line.split()
-            # Format name into one string
-            edited_parts = parts[-5:]
-            edited_parts[0] = " ".join(parts[0:-4])
-            print(edited_parts)
-            projects.append(Project(*edited_parts))
+            # Split line into parts at tab
+            parts = line.strip().split("\t")
+            print(parts)
+            projects.append(Project(*parts))
     print(f"Loaded {len(projects)} projects from {filename}")
     return projects
 
